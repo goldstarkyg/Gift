c37364dee0eced1a3dfca78958bb901b25f60913
goldstarkyg <goldstarkyg91@gmail.com>
2017-11-17 21:13:28 +0800

    add controller for frontend ui

---
+++ gift/app/Http/Controllers/Frontend/FrontendController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Modules\Functions;
+use View;
+
+use Redirect;
+use Response;
+use DB;
+
+class FrontendController extends Controller
+{
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function index(Request $request)
+    {
+        $prod = $request->get('prod', 0);
+        $app_config = array();
+        $app_config['site_url'] = Functions::getSiteURL();
+        $app_config['client_ip'] = Functions::get_client_ip();
+        if( config('app.debug') == false || $prod == 1 )
+            return view('frontend.index_prod', compact('app_config'));
+        else
+            return view('frontend.index', compact('app_config'));
+    }
+}
